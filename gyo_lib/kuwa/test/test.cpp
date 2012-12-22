d4fc5ae6979c10684e4ee546cb6038b67e87f1b2
kotake <shingo014@gmail.com>
2012-12-23 05:50:05 +0900

    小竹修正

--- gyo_lib/kuwa/test/test.cpp
+++ gyo_lib/kuwa/test/test.cpp
@@ -13,70 +13,50 @@ int main()
   cl_platform_id *platforms;
   cl_uint num_platforms;
   cl_int status;
+  //`プラットフォームの数を取得
   status = clGetPlatformIDs(0, NULL, &num_platforms);
   platforms = new cl_platform_id[num_platforms];
+  //プラットフォームIDを取得
   status = clGetPlatformIDs(num_platforms, platforms, NULL);
   cout << "num_platforms : " << num_platforms << endl;
 
   cl_uint num_devices[num_platforms];
   cl_device_id *device_list;
-  
+
+  //各プラットフォームからデバイス数を取得
+  //すべてのプラットフォームのデバイス数の総和を計算
+  cl_uint totaldev = 0;
   for(int i = 0 ; i< num_platforms; i++)
   {
     status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices[i]);
-  }
-
-  cl_uint totaldev = 0;
-  for(int i= 0; i < num_platforms;i++)
-  {
     totaldev += num_devices[i];
   }
-
+  
+  //デバイス数の総和分のメモリ確保
   device_list = new cl_device_id[totaldev];
+  //各プラットフォームのデバイスID, デバイス数を取得、表示
+  //プラットフォームの数だけコンテキスト作成
   cl_uint num;
+  cl_context context[num_platforms];
   for(int i = 0,count=0 ; i< num_platforms; i++)
   {
     status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, num_devices[i], &device_list[count], &num);
-    count+=num;
     cout << "Platform"<<i<<" num_devices : "<<num <<endl;
+  
+    cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platforms[i], 0};
+    context[i] = clCreateContext(properties, num_devices[i], &device_list[count], NULL, NULL, &status);
+    cout << "create context" << i<<" :"<<status<<endl;
+    count += num;
   }
 
+  
+  //総デバイスID表示
   for(int i =0;i<totaldev;i++)
   {
     cout <<"device_list["<<i<<"] : "<<device_list[i]<<endl;
   }
 
-  //cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platforms[0],0};
-  //cl_context_properties properties2[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platforms[1],0};
-
-  //cout << "totaldev : "<<totaldev<<endl;
-
-
-
-  cl_context context[num_platforms];//プラットフォームの数だけコンテキスト作成
-  for(int i=0;i<num_platforms;i++){
-    cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platforms[i],0};
-    context[i] = clCreateContext(properties,num_devices[i],& device_list[i], NULL, NULL, &status); 
-    cout<<"create context"<<i<<" :"<<status<<endl;
-  }
-
-  //context = clCreateContext(properties,1, device_list, NULL, NULL, &status); 
-  //cout<<status<<endl;
-
-  //context2 = clCreateContext(properties2,4,& device_list[1], NULL, NULL, &status);
-
-  //cout << status <<endl;
-
-
-  //for(int i=0;i<totaldev;i++)
-  //  cout<<"device_list: "<<sizeof(device_list)<<"  "<<sizeof(device_list[i])<<endl;
-
-
-
-  // clCreateCommandQueue(context, device_list[0], 0, &status);
-  //
-  //cout <<"CPU context: "<< status <<endl;
-
+  //デバイス数だけコマンドキューを作成
   cl_command_queue queue[totaldev];
   for(int j=0,count=0;j<num_platforms;j++){
     for(int i=0;i<num_devices[j];i++){
